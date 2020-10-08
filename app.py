09ea7a28924aefd7bd44a96041fbad967ec8c8cc
Dustin <dustin.brown.andrews@gmail.com>
2020-10-08 16:26:46 -0600

    Add call out for Flask app

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+# Based on code from:
+# https://github.com/miguelgrinberg/flask-video-streaming
+# Accessed: October 2019
+
+
+
 #!/usr/bin/env python
 from importlib import import_module
 import os
